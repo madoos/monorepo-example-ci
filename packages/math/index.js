64832f919b78036da6e488c8f628521e854a7974
Maurice <maurice.ronet.dominguez@gmail.com>
2020-03-22 22:15:18 +0100

    feat: adds mult method

--- packages/math/index.js
+++ packages/math/index.js
@@ -1,8 +1,12 @@
-const { reduce, add } = require('ramda');
+const { reduce, add, multiply } = require('ramda');
 
 // sum :: [Number] -> Number
 const sum = reduce(add, 0)
 
+// mult :: [Number] -> Number
+const mult = reduce(multiply, 0)
+
 module.exports = {
-    sum
+    sum,
+    mult
 }
