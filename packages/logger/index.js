fec95ae88419a2cd16410d4c36cb8ee53aef996c
Maurice <maurice.ronet.dominguez@gmail.com>
2020-03-22 22:09:18 +0100

    fix(@mono/logger): solve show big objects

--- packages/logger/index.js
+++ packages/logger/index.js
@@ -1,7 +1,17 @@
-const { curry } = require('ramda')
+const { curry, tryCatch, F, ifElse, identity, partialRight } = require('ramda')
+const { parse, stringify } = JSON
+
+// isJSON :: a -> Boolean
+const isJSON = tryCatch(parse, F)
+
+// parseObject :: a -> String | a
+const parseObject = ifElse(isJSON,  partialRight(parse, [null, 2]), identity)
 
 // log :: String -> a -> ()
-const log = curry((tag, data) => console.log(tag, data))
+const log = curry((tag, data) => console.log(
+    tag, 
+    data
+))
 
 module.exports = {
     log
