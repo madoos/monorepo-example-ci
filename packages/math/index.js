8bb153568f688e07c6f84e87ad4ea4f7f2ca1112
Maurice <maurice.ronet.dominguez@gmail.com>
2020-03-22 22:19:49 +0100

    feat: adds pow3 methods

--- packages/math/index.js
+++ packages/math/index.js
@@ -1,10 +1,16 @@
-const { reduce, add, multiply } = require('ramda');
+const { reduce, add, multiply, times, identity, pipe} = require('ramda');
 
 // sum :: [Number] -> Number
 const sum = reduce(add, 0)
 
 // mult :: [Number] -> Number
 const mult = reduce(multiply, 0)
+
+// pow3 :: Number -> Number
+const pow3 = pipe(
+    times(identity),
+    educe(multiply, 1)
+)
 
 module.exports = {
     sum,
